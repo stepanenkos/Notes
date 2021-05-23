fee1d5549943f79a5c405176d08744ee580ce949
Sergey Stepanenko <ssv2408@gmail.com>
2021-05-23 14:32:51 +0600

    рефакторинг

--- app/src/main/java/kz/stepanenkos/notes/editor/presentation/EditorFragment.kt
+++ app/src/main/java/kz/stepanenkos/notes/editor/presentation/EditorFragment.kt
@@ -1,14 +1,13 @@
 package kz.stepanenkos.notes.editor.presentation
 
+import android.content.Context
 import android.os.Bundle
-import android.text.Html
-import android.text.Spannable
-import android.text.SpannableString
-import android.text.style.UnderlineSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.ImageView
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.firestore.FirebaseFirestoreException
@@ -31,18 +30,39 @@ class EditorFragment : Fragment() {
     private lateinit var contentNote: ContentNoteEditText
     private lateinit var doneNote: ImageView
     private lateinit var editNote: ImageView
-    private lateinit var boldButton: ImageView
-    private lateinit var underlinedButton: ImageView
 
     private var noteData: NoteData = NoteData()
-    private var noteDataId = ""
-    private var isChecked = false
-    private var isForEdit = false
-    private var startSelectionIndex: Int = -1
-    private var endSelectionIndex: Int = -1
-    private var isBold = false
-    private var spannableString: SpannableString = SpannableString("")
     private var idNote: String? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        requireActivity().onBackPressedDispatcher.addCallback(
+            this,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    if (isNotBlankTextFields(titleNote, contentNote) &&
+                            noteData.titleNote == titleNote.text.toString() &&
+                            noteData.contentNote == contentNote.text.toString()) {
+                        isEnabled = false
+                        requireActivity().onBackPressed()
+                    } else {
+                        CoroutineScope(Dispatchers.IO).launch {
+                            fillNoteData(titleNote, contentNote)
+                            editorViewModel.saveNote(noteData)
+                        }
+                        doneNoteUI()
+
+                        Snackbar.make(
+                            requireView(),
+                            getString(R.string.editor_fragment_note_saved),
+                            Snackbar.LENGTH_LONG
+                        ).show()
+
+                    }
+                }
+            })
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -53,10 +73,9 @@ class EditorFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_editor, container, false)
         titleNote = root.findViewById(R.id.fragment_editor_title_note)
         contentNote = root.findViewById(R.id.fragment_editor_content_note)
-        doneNote = root.findViewById(R.id.fragment_editor_apply_changed)
-        editNote = root.findViewById(R.id.fragment_editor_edit_text)
-        boldButton = root.findViewById(R.id.fragment_editor_format_bold)
-        underlinedButton = root.findViewById(R.id.fragment_editor_format_underlined)
+        doneNote = root.findViewById(R.id.fragment_editor_apply_changed_button)
+        editNote = root.findViewById(R.id.fragment_editor_edit_text_button)
+
         editNote.disabled()
         doneNote.enabled()
         return root
@@ -65,14 +84,13 @@ class EditorFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         idNote = arguments?.getString("ID")
-        idNote?.let {
-            doneNote.disabled()
+        idNote?.let { noteById ->
             editNote.enabled()
-            editorViewModel.getNoteById(it)
+            doneNote.disabled()
+            editorViewModel.getNoteById(noteById)
         }
 
         editorViewModel.noteById.observe(viewLifecycleOwner, { noteDataInDB ->
-            isForEdit = true
             if (noteDataInDB != null) {
                 noteData = noteDataInDB
                 titleNote.disabled()
@@ -89,128 +107,73 @@ class EditorFragment : Fragment() {
 
     private fun setOnClickListeners() {
         doneNote.setOnClickListener {
-            if(titleNote.text.toString().isBlank() && contentNote.text.toString().isBlank()) {
-                Snackbar.make(requireView(), "Нельзя сохранить пустую заметку", Snackbar.LENGTH_LONG).show()
+            if (!isNotBlankTextFields(titleNote, contentNote)) {
+                Snackbar.make(
+                    requireView(),
+                    getString(R.string.editor_fragment_cannot_save_empty_note),
+                    Snackbar.LENGTH_LONG
+                ).show()
             }
 
-            if (titleNote.text.toString().isNotBlank() &&
-                contentNote.text.toString().isNotBlank() && !isForEdit
-            ) {
-                noteData.titleNote = titleNote.text.toString()
-                noteData.contentNote = contentNote.text.toString()
-                fillSearchKeywordsList(titleNote.text.toString(), contentNote.text.toString())
+            if (isNotBlankTextFields(titleNote, contentNote)) {
+
+                fillNoteData(titleNote, contentNote)
 
                 CoroutineScope(Dispatchers.IO).launch {
                     editorViewModel.saveNote(noteData)
                 }
 
-                titleNote.disabled()
-                contentNote.disabled()
-                doneNote.disabled()
-                editNote.enabled()
-                Snackbar.make(requireView(), "Заметка сохранена", Snackbar.LENGTH_LONG).show()
-            } else if (titleNote.text.toString().isNotBlank() &&
-                contentNote.text.toString().isNotBlank() && isForEdit
-            ){
-                noteData.titleNote = titleNote.text.toString()
-                noteData.contentNote = contentNote.text.toString()
-                noteData.searchKeywords.clear()
-                fillSearchKeywordsList(titleNote.text.toString(), contentNote.text.toString())
+                doneNoteUI()
 
-                editorViewModel.updateNote(noteData)
-                editNote.enabled()
-                titleNote.disabled()
-                contentNote.disabled()
-                doneNote.disabled()
-
-                isForEdit = false
-                Snackbar.make(requireView(), "Заметка обновлена", Snackbar.LENGTH_LONG).show()
-
+                Snackbar.make(
+                    requireView(),
+                    getString(R.string.editor_fragment_note_saved),
+                    Snackbar.LENGTH_LONG
+                ).show()
             }
-
         }
 
         editNote.setOnClickListener {
-            editorViewModel.getNoteById(noteDataId)
-            titleNote.enabled()
-            isForEdit = true
-            contentNote.enabled()
-            doneNote.enabled()
-            editNote.disabled()
+            editNoteUI()
         }
-
-
-        boldButton.setOnClickListener {
-            isBold = !isBold
-/*            val spanHelper = SpanStyleHelper(
-                contentNote,
-                contentNote.text,
-                contentNote.selectionStart,
-                contentNote.selectionEnd
-            )
-            spanHelper.toggleBoldSelectedText()*/
-
-        }
-
-        underlinedButton.setOnClickListener {
-            isChecked = !isChecked
-            spannableString = SpannableString(contentNote.text)
-            startSelectionIndex = contentNote.selectionStart
-            endSelectionIndex = contentNote.selectionEnd
-            val spans = spannableString.getSpans(
-                startSelectionIndex,
-                endSelectionIndex,
-                UnderlineSpan::class.java
-            )
-
-            if (spans.isEmpty()) {
-                spannableString.setSpan(
-                    UnderlineSpan(),
-                    startSelectionIndex,
-                    endSelectionIndex,
-                    Spannable.SPAN_INCLUSIVE_EXCLUSIVE
-                )
-                val toHtml = Html.toHtml(spannableString)
-                contentNote.text?.replace(0, contentNote.text!!.length, Html.fromHtml(toHtml))
-            } else if (spans.isNotEmpty()) {
-                spannableString.removeSpan(spans.first())
-                contentNote.setSelection(endSelectionIndex)
-                val toHtml = Html.toHtml(spannableString)
-                contentNote.text?.replace(0, contentNote.text!!.length, Html.fromHtml(toHtml))
-            }
-        }
-
-
     }
 
-    private fun fillSearchKeywordsList(titleNote: String, contentNote: String) {
-        noteData.searchKeywords.add(titleNote.toLowerCase(Locale.ROOT))
+    private fun doneNoteUI() {
+        titleNote.disabled()
+        contentNote.disabled()
+        doneNote.disabled()
+        editNote.enabled()
+    }
+
+    private fun editNoteUI() {
+        titleNote.enabled()
+        contentNote.enabled()
+        doneNote.enabled()
+        editNote.disabled()
+    }
+    private fun fillNoteData(titleNote: EditText, contentNote: EditText) {
+        if(isNotBlankTextFields(titleNote, contentNote)) {
+            noteData.titleNote = titleNote.text.toString()
+            noteData.contentNote = titleNote.text.toString()
+            noteData.searchKeywords.clear()
+            fillSearchKeywordsList(titleNote, contentNote)
+        }
+    }
+
+    private fun fillSearchKeywordsList(titleNote: EditText, contentNote: EditText) {
+        noteData.searchKeywords.add(titleNote.text.toString().toLowerCase(Locale.ROOT))
         noteData.searchKeywords.addAll(
-            titleNote.toLowerCase(Locale.ROOT).split(Regex("[\\p{Punct}\\s]+"))
+            titleNote.text.toString().toLowerCase(Locale.ROOT).split(Regex("[\\p{Punct}\\s]+"))
         )
 
-        noteData.searchKeywords.add(contentNote.toLowerCase(Locale.ROOT))
+        noteData.searchKeywords.add(contentNote.text.toString().toLowerCase(Locale.ROOT))
         noteData.searchKeywords.addAll(
-            contentNote.toLowerCase(Locale.ROOT).split(Regex("[\\p{Punct}\\s]+"))
+            contentNote.text.toString().toLowerCase(Locale.ROOT).split(Regex("[\\p{Punct}\\s]+"))
         )
-/*        for (index in titleNote.indices) {
-            noteData.searchKeywords.add(titleNote.substring(index).toLowerCase(Locale.ROOT))
-            noteData.searchKeywords.add(titleNote[index].toString())
-        }
+    }
 
-        for(index in titleNote.length downTo 0) {
-            noteData.searchKeywords.add(titleNote.substring(index).toLowerCase(Locale.ROOT))
-        }
-
-        for (index in contentNote.indices) {
-            noteData.searchKeywords.add(contentNote.substring(index).toLowerCase(Locale.ROOT))
-            noteData.searchKeywords.add(contentNote[index].toString())
-
-        }
-
-        for(index in contentNote.length downTo 0) {
-            noteData.searchKeywords.add(contentNote.substring(index).toLowerCase(Locale.ROOT))
-        }*/
+    private fun isNotBlankTextFields(titleNote: EditText, contentNote: EditText): Boolean {
+        return titleNote.text.isNotBlank() && contentNote.text.isNotBlank()
     }
 
     private fun showError(firebaseFirestoreException: FirebaseFirestoreException) {
